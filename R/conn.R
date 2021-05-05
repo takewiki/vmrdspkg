8f4776939f39a80966efe74dcad0125d2dd1535a
takewiki <hulilei@takewiki.com.cn>
2021-05-06 06:13:20 +0800

    add the conn_plm into pkg

--- R/conn.R
+++ R/conn.R
@@ -12,6 +12,21 @@ conn_vm_erp_test_aux <- function() {
 }
 
 
+#' 测试环境PLM的连接参数设置
+#'
+#' @return 返回链接信息
+#'
+#' @examples
+#'  conn_vm_plm_test_aux()
+conn_vm_plm_test_aux <- function() {
+  res <-tsda::sql_conn_common(ip = '123.207.201.140',user_name = 'sa',password = 'rds@2020',db_name = 'TC4K3DB')
+  return(res)
+
+}
+
+
+
+
 #' VM公司的测试环境的ERP设置
 #'
 #' @return 返回测试链接
@@ -22,6 +37,22 @@ conn_vm_erp_test_aux <- function() {
 conn_vm_erp_test <- function() {
   #获取链接信息
   res <-conn_vm_erp_test_aux()
+  return(res)
+
+}
+
+
+
+#' VM公司的测试环境的PLM设置
+#'
+#' @return 返回测试链接
+#' @export
+#'
+#' @examples
+#' conn_vm_plm_test()
+conn_vm_plm_test <- function() {
+  #获取链接信息
+  res <-conn_vm_plm_test_aux()
   return(res)
 
 }
