e0bb6c8811192c616de375c6a3720b80057f8e10
takewiki <hulilei@takewiki.com.cn>
2021-06-01 19:06:51 +0800

    updae the files.

--- R/util.R
+++ R/util.R
@@ -25,15 +25,47 @@ order by FClassIndex")
 }
 
 
-#' 设置BOM单号的最大流水号
+
+#' 获取BOM单号流水号的最大值并转化为数值型
 #'
-#' @param conn
+#' @param conn 连接
 #'
-#' @return
+#' @return 返回值
 #' @export
 #'
 #' @examples
-BOM_setNewBillNo <- function(conn=conn_vm_erp_test()) {
+#' BOM_getMaxBillValue()
+BOM_getMaxBillValue <- function(conn=conn_vm_erp_test()) {
+
+  sql <- paste0("select    FProjectVal   from t_BillCodeRule  a where FBillTypeID =50
+and FProjectID =3")
+  r <- tsda::sql_select(conn,sql)
+  #获取的是字符串，然后返回是数值型
+  res = as.integer(r$FProjectVal)
+
+
+  return(res)
+}
+
+#' 设置BOM单号的最大流水号
+#'
+#' @param conn 连接
+#' @param ncount 数量
+#'
+#' @return 返回值
+#' @export
+#'
+#' @examples
+#' BOM_setNewBillNo()
+BOM_setNewBillNo <- function(conn=conn_vm_erp_test(),ncount=1) {
+  #获取最大号
+  value = BOM_getMaxBillValue(conn = conn)
+  next_value =  value + ncount
+  value_next2 =  as.character(next_value)
+  #更新相应的BOM号吗
+  sql_update <- paste0("update a set FProjectVal ='",value_next2,"'  from t_BillCodeRule  a where FBillTypeID =50
+and FProjectID =3")
+  tsda::sql_update(conn,sql_str = sql_update)
 
 
 }
@@ -58,38 +90,78 @@ BOM_getNewInterId <- function(conn=conn_vm_erp_test()) {
 
 #' 获取BOM新增的模板_表体部分
 #'
+#' @param BOMRevCode 子项BOM版本号
+#' @param PLMBatchnum  批号
+#' @param FLowCode  流程低位码
 #' @param conn 连接
-#' @param data_bom 新增的BOM数据
 #'
 #' @return 返回值
 #' @export
 #'
 #' @examples
-#' BOM_getNewBillTpl()
-BOM_getNewBillTpl_Body <- function(conn=conn_vm_erp_test(),data_bom) {
-     #思路可以有
+#' BOM_getNewBillTpl_Body()
+BOM_getNewBillTpl_Body <- function(conn=conn_vm_erp_test(),
+                                   BOMRevCode='TCB100181/A',
+                                   PLMBatchnum='BOM00000002',
+                                   FLowCode =2
 
-   sql <- paste0("select * from  rds_ICBomChild_tpl")
+                                   ) {
+     #思路可以有
+  #获取模板数据
+   sql <- paste0("select * from  rds_icbom_tpl_body")
    data_tpl <- tsda::sql_select(conn,sql)
+   #print(length(names(data_tpl)))
+   #获取实际数据
+   sql_bom <- paste0("select FSubItemId as FItemID,FSubUnitId as FUnitID,BOMCount as FQty
+   from  [vw_PLMtoERP_BOM]
+  where BOMRevCode='",BOMRevCode,"' and PLMBatchnum='",PLMBatchnum,"'
+  and CMCode <>'' and FLowCode = ",FLowCode)
+   data_bom <- tsda::sql_select(conn,sql_bom)
+
    ncount <- nrow(data_bom)
-   data_p <- tsdo::df_rowRepMulti(data_tpl,times = ncount)
-   #针对数据进行替换
-   data_p$FItemID <- data_bom$FItemID
-   data_p$FEntryID <- 1:ncount
-   data_p$FInterID <- rep(BOM_getNewInterId(conn = conn),ncount)
-   data_p$FUnitID <- data_bom$FUnitID
-   data_p$FQty <- data_bom$FQty
-   data_p$FAuxQty <- data_bom$FQty
+
    #上传数据库
    if(ncount >0){
-     tsda::db_writeTable(conn = conn,table_name = 'ICBomChild',r_object = data_p,append = T)
+     #检验实际获得的数据
+     data_p <- tsdo::df_rowRepMulti(data_tpl,times = ncount)
+     #针对数据进行替换
+     data_p$FItemID <- data_bom$FItemID
+     data_p$FEntryID <- 1:ncount
+     data_p$FInterID <- rep(BOM_getNewInterId(conn = conn),ncount)
+     data_p$FUnitID <- data_bom$FUnitID
+     data_p$FQty <- as.numeric(data_bom$FQty)
+     data_p$FAuxQty <- as.numeric(data_bom$FQty)
+     data_p$FPDMImportDate <-''
+     data_p$FEntrySelfZ0144 <- 0
+     data_p$FEntrySelfZ0145 <- 0
+     #View(data_p)
+     openxlsx::write.xlsx(data_p,'data_bom.xlsx')
+     str(data_p)
+     #写入BOM缓存表
+     tsda::db_writeTable(conn = conn,table_name = 'rds_icbomChild_input',r_object = data_p,append = T)
+     #将数据写入正式表
+     sql_write_bom_body <- paste0("INSERT INTO ICBomChild (FInterID,FEntryID,FBrNo,FItemID,FAuxPropID,FUnitID,FMaterielType,FMarshalType,FQty,FAuxQty,FBeginDay,FEndDay,FPercent,FScrap,FPositionNo,FItemSize,FItemSuite,FOperSN,FOperID,FMachinePos,FOffSetDay,FBackFlush,FStockID,FSPID,FNote,FNote1,FNote2,FNote3,FPDMImportDate,FDetailID,FCostPercentage,FEntrySelfZ0142,FEntrySelfZ0144,FEntrySelfZ0145,FEntrySelfZ0146,FEntrySelfZ0148)
+select *   from rds_icbomChild_input ")
+     tsda::sql_update(conn,sql_write_bom_body)
+     #清空缓存表
+     sql_clear_bom_body_input <- paste0("truncate table  rds_icbomChild_input ")
+     tsda::sql_update(conn,sql_clear_bom_body_input)
+
    }
 
 }
 
 
 
-BOM_getNewBillTpl_Head <- function(conn=conn_vm_erp_test(),data_bom) {
+BOM_getNewBillTpl_Head <- function(conn=conn_vm_erp_test(),
+                                   BOMRevCode='TCB100181/A',
+                                   PLMBatchnum='BOM00000002',
+                                   FLowCode =2) {
+  #获取模板数据
+  sql <- paste0("select * from  rds_icbom_tpl_body")
+  data_tpl <- tsda::sql_select(conn,sql)
+  #
+
 
 
 
--- metadata/bom_insert/bom_write2ERP.sql
+++ metadata/bom_insert/bom_write2ERP.sql
@@ -10,6 +10,11 @@ update a set FProjectVal ='4907'  from t_BillCodeRule  a where FBillTypeID =50
 and FProjectID =3
 
 
+select    FProjectVal   from t_BillCodeRule  a where FBillTypeID =50
+and FProjectID =3
+
+
+
 
 
 
@@ -51,7 +56,7 @@ into rds_icbom_tpl_body
 from ICBOMChild
 where finterid =6499
 
-
+select *      from rds_icbom_tpl_body
 
 select *  into  rds_icbomChild_input    from rds_icbom_tpl_body
 
@@ -59,6 +64,122 @@ select *  into  rds_icbomChild_input    from rds_icbom_tpl_body
 select *    into  rds_icbom_input   from rds_icbom_tpl_head
 
 
-select * from rds_icbomChild_input
+delete  from rds_icbomChild_input
+
+
+select *   from rds_icbomChild_input
+
+
+select * from  rds_icbom_tpl_head
+
+
+
+
+INSERT INTO ICBomChild (FInterID,FEntryID,FBrNo,FItemID,FAuxPropID,FUnitID,FMaterielType,FMarshalType,FQty,FAuxQty,FBeginDay,FEndDay,FPercent,FScrap,FPositionNo,FItemSize,FItemSuite,FOperSN,FOperID,FMachinePos,FOffSetDay,FBackFlush,FStockID,FSPID,FNote,FNote1,FNote2,FNote3,FPDMImportDate,FDetailID,FCostPercentage,FEntrySelfZ0142,FEntrySelfZ0144,FEntrySelfZ0145,FEntrySelfZ0146,FEntrySelfZ0148)
+select *   from rds_icbomChild_input
+go
+
+
+truncate table  rds_icbomChild_input
+
+
+select *   from ICBOMChild where FInterID =6500
+
+
+
+
 
 select *  from  rds_icbom_input
+
+
+
+select * from  rds_icbom_tpl_body
+
+
+
+sp_help rds_icbomChild_input
+
+------同步数据
+
+    select *  into PLMtoERP_BOM   from [TC4K3DB].[dbo].[PLMtoERP_BOM]
+
+
+------创建视图
+create view vw_PLMtoERP_BOM as
+select a.*,i_pm.FItemID as FParentItemId, i_pm.FUnitID as FParentUnitID,
+     i_sub.FItemID as FSubItemId,i_sub.FUnitID as FSubUnitId
+
+from  [PLMtoERP_BOM] a
+left  join t_ICItem i_pm
+on  a.PMCode collate chinese_prc_ci_as  =  i_pm.FNumber
+left  join t_ICItem i_sub
+on  a.CMCode  collate chinese_prc_ci_as  = i_sub.FNumber
+go
+
+
+
+
+
+
+
+	  select *   from  [vw_PLMtoERP_BOM]
+  where  PLMBatchnum='BOM00000002'
+
+
+
+create table rds_BOM_version (FVersion_PLM varchar(30),FVersion_ERP varchar(30))
+insert into rds_BOM_version values('A','001')
+insert into rds_BOM_version values('B','002')
+insert into rds_BOM_version values('C','003')
+insert into rds_BOM_version values('D','004')
+insert into rds_BOM_version values('E','005')
+insert into rds_BOM_version values('F','006')
+insert into rds_BOM_version values('G','007')
+insert into rds_BOM_version values('H','008')
+insert into rds_BOM_version values('I','009')
+insert into rds_BOM_version values('J','010')
+insert into rds_BOM_version values('K','011')
+insert into rds_BOM_version values('L','012')
+insert into rds_BOM_version values('M','013')
+insert into rds_BOM_version values('N','014')
+insert into rds_BOM_version values('O','015')
+insert into rds_BOM_version values('P','016')
+insert into rds_BOM_version values('Q','017')
+insert into rds_BOM_version values('R','018')
+insert into rds_BOM_version values('S','019')
+insert into rds_BOM_version values('T','020')
+insert into rds_BOM_version values('U','021')
+insert into rds_BOM_version values('V','022')
+insert into rds_BOM_version values('W','023')
+insert into rds_BOM_version values('X','024')
+insert into rds_BOM_version values('Y','025')
+insert into rds_BOM_version values('Z','026')
+
+
+
+select * from rds_BOM_version
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	  select FParentItemId,FParentUnitID,BOMRevCode,ProductGroup   from  [vw_PLMtoERP_BOM]
+  where PLMBatchnum='BOM00000002'
+and PMCode='2.104.20.00034' and CMCode =''
+
+
+select * from ICBOMGroup where FName ='104'
